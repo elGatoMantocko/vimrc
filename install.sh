d21fbcdd83581a50cf8f7af662cddc02fcb16699
Elliott Mantock <emantock@egencia.com>
2018-08-31 09:28:45 -0700

    updated vim plugin installation (for reruns)

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ mkdir -p ~/.vim/bundle
 [ ! -e ~/.vim/bundle/Vundle.vim ] && git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
 
 ln -sf $(dirname $0:a)/.vimrc ~/.vimrc
-vim +PluginInstall +qall
+vim +PluginClean +PluginInstall +PluginUpdate +qall
 cd ~/.vim/bundle/tern_for_vim && npm i
 cd ~/.vim/bundle/YouCompleteMe && python3 install.py --clang-completer --java-completer --js-completer
