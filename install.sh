eaeb19402a28435f7f5d9bcbd33fe377a10129e5
Elliott Mantock <elliott@kaskada.com>
2020-01-16 11:05:34 -0800

    mv project .vimrc to vimrc

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 mkdir -p ~/.vim/bundle
 [ ! -e ~/.vim/bundle/Vundle.vim ] && git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
 
-ln -sf $(dirname $0:a)/.vimrc ~/.vimrc
+ln -sf $(dirname $0:a)/vimrc ~/.vimrc
 vim +PluginClean +PluginInstall +PluginUpdate +qall
 cd ~/.vim/bundle/YouCompleteMe && python3 install.py --clang-completer --java-completer --js-completer --ts-completer --go-copmleter
